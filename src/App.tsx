d13b0821c64a6e2a4cf0eaef4fdf146f306683e4
dzordii <dzordinetwork@gmail.com>
2025-03-08 02:06:09 -0300

    first commit, and init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="flex h-screen justify-center bg-black text-gray-500 items-center">
+        Hello World!
+      </h1>
+    </>
+  );
+}
